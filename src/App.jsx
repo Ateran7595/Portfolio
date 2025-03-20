c4a50b435641906cb8e4d426ffd603dd4292e554
Ateran7595 <alexteran795@gmail.com>
2025-03-19 21:39:31 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div className='bg-orange-200'>
+      <Button>Funciono :O</Button>
+    </div>
+  )
+}
+
+export default App
