ad4c9c6c51030e1d59e0deea07ed068d5e359a6d
Ateran7595 <alexteran795@gmail.com>
2025-03-20 20:53:35 -0600

    Added animation background and new stylings

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { Button } from './components/ui/button'
+import './App.css'
 
 function App() {
 
   return (
-    <div className='bg-orange-200'>
-      <Button>Funciono :O</Button>
+    <div className="lines">
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className='flex items-center justify-center mt-[250px] font-bold'>
+        <Button className='font-bold'>Funciono :O</Button>
+      </div>
+      
     </div>
+
   )
 }
 
