09aecc9ebd3d36fc7fdb5a3c6eca989fcdf1278d
Ateran7595 <alexteran795@gmail.com>
2025-03-22 20:09:40 -0600

    Added skils section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,20 @@ import { Button } from './components/ui/button'
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { IoIosMenu } from "react-icons/io";
+import { FaReact } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3 } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaNodeJs } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+import { BiLogoMongodb } from "react-icons/bi";
+import { RiFirebaseFill } from "react-icons/ri";
+import { FaGitAlt } from "react-icons/fa";
+import { TbApi } from "react-icons/tb";
+import { RiGeminiFill } from "react-icons/ri";
+
+
 import {
   Sheet,
   SheetClose,
@@ -13,6 +27,7 @@ import {
   SheetTrigger,
 } from './components/ui/sheet'
 import './App.css'
+import Tools from './ReuseComp/Tools';
 
 function App() {
 
@@ -73,8 +88,37 @@ function App() {
           Through the projects I've built, I focus on delivering smooth user experiences and intuitive navigation. I'm actively seeking new opportunities to apply the skills I've acquired on my journey as a developer. Looking forward to connecting with you!
         </p>
       </div>
-      <div>
-
+      <div id='Skills' className='relative flex flex-col justify-center items-start font-roboto w-[1100px] mt-[100px] bg-[#ffccba94] shadow-shadow rounded-[5px] p-4 border-solid border-2 border-black'>
+        <h1 className='font-bold text-[38px]'>Skills</h1>
+        <div>
+          <div>
+            <h1 className='font-semibold text-[25px]'>Frontend:</h1>  
+            <div className='flex flex-wrap gap-4'>
+              <Tools icon={FaReact} name='ReactJS' desc='JavaScript Library' iconClass='text-blue-500' />
+              <Tools icon={FaHtml5} name='HTML' desc='Markup Language' iconClass='text-orange-600' />
+              <Tools icon={FaCss3} name='CSS' desc='Styling Language' iconClass='text-blue-600' />
+              <Tools icon={IoLogoJavascript} name='JavaScript' desc='Programming Language' iconClass='text-yellow-300' />
+              <Tools icon={RiTailwindCssFill} name='TailwindCSS' desc='CSS Framework' iconClass='text-blue-500' />
+            </div>
+          </div>
+          <div>
+            <h1 className='font-semibold text-[25px] mt-5'>Backend & Databases:</h1>
+            <div className='flex flex-wrap gap-4'>
+              <Tools icon={FaNodeJs} name='Node.JS' desc='JavaScript Runtime' iconClass='text-green-500' />
+              <Tools icon={SiExpress} name='Express' desc='Web Framework' iconClass='text-black' />
+              <Tools icon={BiLogoMongodb} name='MongoDB' desc='NoSQL Database' iconClass='text-green-400' />
+              <Tools icon={RiFirebaseFill} name='Firebase' desc='Backend Platform' iconClass='text-yellow-400' />
+            </div>
+          </div>
+          <div>
+            <h1 className='font-semibold text-[25px] mt-5'>Others:</h1>
+            <div className='flex flex-wrap gap-4'>
+              <Tools icon={FaGitAlt} name='Git' desc='Version Control' iconClass='text-orange-500' />
+              <Tools icon={TbApi} name='APIs' desc='Backend Tools' iconClass='text-black' />
+              <Tools icon={RiGeminiFill} name='Gemini AI' desc='AI model' iconClass='text-blue-500' />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
---
+++ src/ReuseComp/Tools.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Tools({ icon: Icon, name, desc, iconClass}) {
+  return (
+    <div className='flex justify-center items-center gap-1 bg-[#ff9ea2]  rounded-[5px] p-2 border-solid border-2 border-black'>
+        <Icon className={`text-[35px] ${iconClass}`} />
+        <div className='flex flex-col leading-tight'>
+            <h1 className='font-semibold text-[20px]'>{name}</h1>
+            <p className='text-[15px]'>{desc}</p>
+        </div>
+    </div> 
+  )
+}
+
+export default Tools
